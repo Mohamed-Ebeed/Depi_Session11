a09b4e48feddafc5cdc0d951d798020fdc256cea
Mohamed Ahmed <ebaidmedo101@gmail.com>
2026-09-24 17:50:00 +0300

    Add Order class with properties for Id, CustomerName, Price, and Quantity

---
+++ Depi_Session11/Clsorder.cs
@@ -0,0 +1,10 @@
+namespace Depi_Session11
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
